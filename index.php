3cb0c117640c03aa8ff1ccd503b50c3b5e6acc5d
Tomoaki -imak5k- <tomoaki.nagahara+imac5k@gmail.com>
2017-12-06 17:20:59 +0900

    NEW: ROMANCE DAWN!!

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * app-skeleton-3:/html/reference/index.php
+ *
+ * @creation  2017-09-10
+ * @version   1.0
+ * @package   app-skeleton
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+Template::Run('index.phtml');
