fedeb5ecaab1e4d02ed2702930a67907281dad4c
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-02-08 11:42:26 +0900

    New: api > common.php

---
+++ api/common.php
@@ -0,0 +1,19 @@
+<?php
+/** op-module-reference:/api/common.php
+ *
+ * @created   2024-02-07
+ * @version   1.0
+ * @package   op-module-reference
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+ /** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP\MODULE\REFERENCE;
