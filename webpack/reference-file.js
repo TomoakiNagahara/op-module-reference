845552c515154f07bbcf8b73833147713852c17e
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-02-12 06:56:28 +0900

    2024: webpack > reference-file.js

---
+++ webpack/reference-file.js
@@ -0,0 +1,37 @@
+
+/** op-module-reference:/reference-file.js
+ *
+ * @created   2024-02-08
+ * @version   1.0
+ * @package   op-module-reference
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+//	...
+if( typeof $OP === 'undefined' ){
+	$OP = {};
+}
+
+//	...
+if( typeof $OP.Reference === 'undefined' ){
+	$OP.Reference = {};
+}
+
+//	...
+if( typeof $OP.Reference.File === 'undefined' ){
+	$OP.Reference.File = async function File($path){
+		//	...
+		const URL      = "./api/file/" + '?path=' + $path;
+		const response = await fetch(URL);
+		const json     = await response.json();
+
+		//	...
+		document.getElementById('markdown').innerHTML = marked.parse(json.result);
+	};
+}
+
+//	...
+document.addEventListener('DOMContentLoaded', function(){
+	$OP.Reference.File('');
+});
