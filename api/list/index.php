5815f022dc0f525b5b928474b05f3580c409b569
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-02-15 17:01:05 +0900

    WIP: api > list > index.php

--- api/list/index.php
+++ api/list/index.php
@@ -22,17 +22,40 @@ namespace OP\MODULE\REFERENCE;
 include('../common.php');
 
 //	...
-$path = OP()->Request('path');
+$request = OP()->Api()->Request();
+$meta = $request['meta'] ?? null;
+$name = $request['name'] ?? null;
+//$dir  = $request['dir'];
 
 //	...
-if( $path ){
-	$result = [];
+if(!$meta ){
+	OP()->Api()->Error("Empty meta.");
 }else{
-	$result = ['OP','Unit'];
+	//	...
+	if( $meta === 'core' ){
+		$path = "{$meta}:/reference/*";
+	}else{
+		$path = $name ? "{$meta}:/{$name}/reference/*": "{$meta}:/*";
+	}
+	D($path);
+
+	//	...
+	if( $path = OP()->MetaPath($path) ){
+		foreach( glob($path) as $path ){
+			$name = basename($path);
+			list($name, $ext) = explode('.', $name);
+			$result[] = [
+				'dir'  => is_dir($path),
+				'name' => $name,
+				'md'   => ($ext === 'md') ? true: false,
+			];
+		}
+	}
+	D($path);
 }
 
 //	...
-OP()->Api()->Result($result);
+OP()->Api()->Result($result ?? []);
 
 //	...
 OP()->Api()->Out();
