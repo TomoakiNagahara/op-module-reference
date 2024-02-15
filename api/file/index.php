9a0df8138ef2c6c8a2ce0e539d18c3fb94ce863e
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-02-16 00:31:27 +0900

    Fix: api > file > index.php

--- api/file/index.php
+++ api/file/index.php
@@ -21,22 +21,58 @@ namespace OP\MODULE\REFERENCE;
 //	...
 include('../common.php');
 
-/* @var $api \OP\UNIT\Api */
-$api = OP()->Api();
-
 //	...
-$request = $api->Request();
+$request = OP()->Request();
 
 //	...
 if(!$path = $request['path'] ?? null ){
 	$path = 'reference:/README.md';
+}else{
+	/* @var $match array */
+	if(!preg_match('|^([a-z]+):/([^/]*)/?(.*)|', $path, $match) ){
+		//	...
+		OP()->Api()->Error("Unmatch path. ($path)");
+	}else{
+		//	...
+		$meta = $match[1] ?? null;
+		$name = $match[2] ?? null;
+		$file = $match[3] ?? null;
+
+		//	...
+		if( $meta === 'core' ){
+			if(!$name ){
+				$path = "{$meta}:/README";
+			}else{
+				$path = $file ? "{$meta}:/reference/{$name}/{$file}": "{$meta}:/reference/{$name}";
+			}
+		}else if( $name ){
+			//	Unit, Layout, Module
+			$path = $file ? "{$meta}:/{$name}/reference/{$file}": "{$meta}:/{$name}/README";
+		}else{
+			$path = '';
+		}
+	}
 }
 
 //	...
-$content = file_get_contents( OP()->MetaPath($path) );
+if(!$path){
+	//	...
+}else if(!$real_path = OP()->MetaPath($path) ){
+	//	...
+	OP()->Api()->Error("Can not convert this path. ($path)");
+}else{
+	//	...
+	$real_path .= '.md';
+
+	//	...
+	if(!file_exists($real_path) ){
+		//	...
+		OP()->Api()->Error("File does not exists. ($real_path)");
+	}else{
+		//	...
+		OP()->Api()->Result( file_get_contents($real_path) );
+	}
+}
 
 //	...
-$api->Result( $content );
-
-//	...
-$api->Out();
+OP()->Api()->Out();
